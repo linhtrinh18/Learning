d831a245ebc8035a25e32c8d73c0768a7a9ef78b
EC2 Default User <ec2-user@ip-172-31-46-86.us-east-2.compute.internal>
2019-05-20 12:51:20 +0000

    my first commit

---
+++ react-redux/reduxAnalogy/reduxExample.js
@@ -0,0 +1,84 @@
+// This is the redux example file:
+const Redux = require('./reduxCdn')
+const { createStore, combineReducers } = Redux;
+
+// People dropping off forms (Action Creators)
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return { // This is the form
+    type: 'CREATE_CLAIM',
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect
+    }
+  };
+};
+
+const createPolicy = (name) => {
+  return {
+    type: 'CREATE_POLICY',
+    payload: {
+      name: name,
+      amount: 20
+    }
+  }
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: 'DELETE_POLICY',
+    payload: {
+      name: name
+    }
+  }
+};
+
+// Departments (reducers)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if(action.type === 'CREATE_CLAIM') {
+    return [...oldListOfClaims, action.payload];
+  }
+  
+  return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if(action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if(action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amount;
+  }
+  
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if(action.type === 'CREATE_POLICY') {
+    return [...listOfPolicies, action.payload.name]
+  } else if(action.type === 'DELETE_POLICY') {
+    return listOfPolicies.filter(policy => policy != action.payload.name);
+  }
+  
+  return listOfPolicies;
+}
+
+// Company setup
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies
+});
+
+const store = createStore(ourDepartments);
+
+store.dispatch(createPolicy('Alex'));
+store.dispatch(createClaim('Alex', 10));
+store.dispatch(deletePolicy('Alex'));
+
+store.dispatch(createPolicy('Jimmy'));
+store.dispatch(createClaim('Alex', 70));
+
+store.dispatch(createPolicy('Linh'));
+store.dispatch(createClaim('Linh', 20));
+
+console.log(store.getState());
+console.log(JSON.stringify(store))
