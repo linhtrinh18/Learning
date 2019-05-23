71f5188b55046a33fbd8ae262cd02d6348899c6c
thien thuan <thienthuan171@gmail.com>
2019-05-23 04:54:31 +0000

    fetch all the route of stream in RESTFUL convention

--- react-redux/streamvideo/client/src/actions/index.js
+++ react-redux/streamvideo/client/src/actions/index.js
@@ -1,5 +1,12 @@
 import streams from '../apis/streams'
-import { SIGN_IN, SIGN_OUT, CREAT_STREAM } from './types'
+import {   SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM }
+from './types'
 
 
 export const signIn = (id) => {
@@ -16,7 +23,39 @@ export const signOut = () => {
 }
 
 
-export const createStream = formValues =>async dispatch => {
-    const response = await streams.post('/streams', formValues)
-    dispatch({type:CREAT_STREAM, payload: response.data})
-}
+// export const createStream = formValues =>async dispatch => {
+//     const response = await streams.post('/streams', formValues)
+//     dispatch({type: CREATE_STREAM, payload: response.data})
+// }
+
+
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.patch(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
--- react-redux/streamvideo/client/src/apis/streams.js
+++ react-redux/streamvideo/client/src/apis/streams.js
@@ -3,5 +3,4 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://project-thienthuan171.c9users.io:8081'
-    // baseURL: 'https://steven-grinder-thienthuan171.c9users.io:3000'
 })
