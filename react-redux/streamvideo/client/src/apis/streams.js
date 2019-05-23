38b69f213518e02ea336b72947292d4e65bc667f
thien thuan <thienthuan171@gmail.com>
2019-05-23 04:12:37 +0000

    successfully create json-server on cloud9

---
+++ react-redux/streamvideo/client/src/apis/streams.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+
+export default axios.create({
+    baseURL: 'https://project-thienthuan171.c9users.io:8081'
+    // baseURL: 'https://steven-grinder-thienthuan171.c9users.io:3000'
+})
--- react-redux/streamvideo/client/src/components/streams/StreamCreate.js
+++ react-redux/streamvideo/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux';
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component {
     renderError({error, touched}) {
@@ -24,8 +26,8 @@ class StreamCreate extends React.Component {
     }
     
     
-    onSubmit(formValues) {
-        console.log(formValues)
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
     }
     
     
@@ -57,7 +59,14 @@ const validate = (formValues) => {
 }
 
 
-export default reduxForm({
+// export default reduxForm({
+//     form: 'streamCreate',
+//     validate: validate
+// })(StreamCreate)
+
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate: validate
-})(StreamCreate)
+})(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped)
