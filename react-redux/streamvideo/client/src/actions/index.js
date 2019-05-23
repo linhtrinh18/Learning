53df38ddc3d19fa8a1aaedddbfeb16f0fc99107f
thien thuan <thienthuan171@gmail.com>
2019-05-23 09:18:52 +0000

    render the Stream list on screen

--- react-redux/streamvideo/client/src/actions/index.js
+++ react-redux/streamvideo/client/src/actions/index.js
@@ -30,8 +30,8 @@ export const signOut = () => {
 
 
 export const createStream = formValues => async (dispatch, getState) => {
-  const { userId } = getState().auth;
-  const response = await streams.post('/streams', { ...formValues, userId });
+  // const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues });
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
